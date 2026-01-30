5ef6f7e5e8f0894e7223bb70dfdf926e9c7ffe44
Matous Kovar <matouskovar1@gmail.com>
2026-01-30 08:05:03 +0100

    better practices for keeping connection string

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using System.Data;
+using SimpleAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace SimpleAPI;
 
@@ -6,13 +9,20 @@ public class Program
 {
      public static void Main(string[] args)
     {
+       
         var builder = WebApplication.CreateBuilder(args);
+
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")  // sets from connection string from config file. AddDbContext is handled by MachineDbContext constructor
+            ?? throw new InvalidOperationException("Connection string"
+            + "'DefaultConnection' not found.");
+        builder.Services.AddDbContext<MachineDbContext>(options => options.UseSqlServer());
 
         builder.Services.AddEndpointsApiExplorer();
 
         builder.Services.AddSwaggerGen();        
 
         builder.Services.AddControllers(); // scans directory for classes with ending Controller
+
 
         var app = builder.Build();
 
