a305aa3d5c04873004069a189aa457fa6db09f75
Matous Kovar <matouskovar1@gmail.com>
2026-02-23 18:46:18 +0100

    tests working

--- SimpleAPI.Tests/Controllers/CustomWebApplicationFactory.cs
+++ SimpleAPI.Tests/Controllers/CustomWebApplicationFactory.cs
@@ -2,38 +2,61 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
 using SimpleAPI.Data;
 using SimpleAPI.Models;
 
 namespace SimpleAPI.Tests.Controllers;
 
-// Creates database with basic data
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+    
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
         {
+            // 1. Smažeme původní nastavení (to už známe)
             var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<MachineDbContext>));
             if (descriptor != null) services.Remove(descriptor);
 
+            
+            var inMemoryServiceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+            
             services.AddDbContext<MachineDbContext>(options => 
-                options.UseInMemoryDatabase("GlobalSharedTestDb"));
+            {
+                options.UseInMemoryDatabase("GlobalSharedTestDb");
+                options.UseInternalServiceProvider(inMemoryServiceProvider); // <--- TOTO ŘEŠÍ TVŮJ PROBLÉM
+            });
+        });
+    }
+    
+    
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        // Necháme .NET postavit celou aplikaci (tím se finálně aplikuje In-Memory)
+        var host = base.CreateHost(builder);
 
-            
-            var serviceProvider = services.BuildServiceProvider();
-            using var scope = serviceProvider.CreateScope();
-            
+        // Teď už můžeme bezpečně vytáhnout databázi a naplnit ji
+        using (var scope = host.Services.CreateScope())
+        {
             var db = scope.ServiceProvider.GetRequiredService<MachineDbContext>();
-                
             
+            db.Database.EnsureCreated();
+                
+            // Vložíme naše základní data
             if (!db.Machines.Any())
             {
                 db.Machines.Add(new Machine { Id = 1, Code = "M1", Status = "active" });
-                db.Operators.Add(new Operator { Id = 1, FirstName = "Jan", LastName = "Novak" });
+                db.Operators.Add(new Operator { Id = 1, FirstName = "Jan", LastName = "Novak",BadgeNumber = "C1"});
                 db.Projects.Add(new Project { Id = 1, Name = "Valid Project", Status = "planned" });
+                
                 db.SaveChanges(); 
             }
-        });
+        }
+
+        return host;
     }
 }
